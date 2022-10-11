4b8041ccd0c46a3df65791ac9bfb9481d89d5f56
jithinkmadhu <jithinmadhu00@gmail.com>
2022-10-11 17:18:25 +0530

    Update OwnerController.java

--- src/main/java/com/orthofx/owner/owner/OwnerController.java
+++ src/main/java/com/orthofx/owner/owner/OwnerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -46,7 +47,7 @@ public class OwnerController {
 		return this.ownerRepository.save(owner);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/owners/{id}")
+	@PutMapping("/owners/{id}")
 	public ResponseEntity<Owner> updateOwner(@PathVariable(value = "id") Long ownerId, @Validated @RequestBody Owner ownerDetails) throws ResourceNotFoundException{
 		Owner owner = ownerRepository.findById(ownerId).orElseThrow(() -> new ResourceNotFoundException("Owner not found for this id :: " + ownerId));
 		
